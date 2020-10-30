265e2a57ce8321057feecdc018ae1b0b6f9fb2ef
bedimcode <61248995+bedimcode@users.noreply.github.com>
2020-10-30 02:26:27 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+/*===== SHOW MENU =====*/
+
+
+/*===== REMOVE MENU MOBILE =====*/
+
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+
+
+/*===== CHANGE BACKGROUND HEADER =====*/ 
+
+
+/*===== SHOW SCROLL TOP =====*/ 
+
+
+/*===== MIXITUP FILTER PORTFOLIO =====*/ 
+
+/* Link active portfolio */ 
+
+
+/*===== SWIPER CAROUSEL =====*/ 
+
+
+/*===== GSAP ANIMATION =====*/ 
+
+
